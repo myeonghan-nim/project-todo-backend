0d9b78600fc8c8af1c37ffca82da8f3021a6bf13
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-11-19 11:52:23 +0900

    Add: todo create

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+
+from .serializers import TodoSerializer
+
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 # Create your views here.
+
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated, ))
+@authentication_classes((JSONWebTokenAuthentication, ))
+def todo_create(request):
+
+    serializer = TodoSerializer(data=request.POST)
+    if serializer.is_valid():
+        serializer.save()
+
+        return JsonResponse(serializer.data)
+
+    return HttpResponse(status=400)
