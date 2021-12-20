338daa267a12d38fc19bc566900eff4700fbd8fc
myeonghan-nim <ataraxia9408@gmail.com>
2021-12-20 14:57:27 +0900

    Delete: needless blank

--- todo/models.py
+++ todo/models.py
@@ -1,16 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
+from django.db import models
 
 
 class User(AbstractUser):
-    
     pass
 
 
 class Todo(models.Model):
-    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
--- todo/serializers.py
+++ todo/serializers.py
@@ -1,18 +1,15 @@
 from rest_framework import serializers
+
 from .models import Todo, User
 
 
-# serializer for deal todo
 class TodoSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Todo
         fields = ('id', 'user', 'title', 'completed', )
 
 
-# serializer for deal todo list
 class UserSerializer(serializers.ModelSerializer):
-
     todo_set = TodoSerializer(many=True)
 
     class Meta:
