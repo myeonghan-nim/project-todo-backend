99d4d8abfc8548b2bb28e5fc708a9c8a47148fbf
myeonghan-nim <ataraxia9408@gmail.com>
2021-12-20 14:57:42 +0900

    Fix: uncorrect logic

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('todo/', views.todo_create),
     path('todo/<int:todo_id>/', views.todo_detail),
-    path('users/<int:user_id>/', views.user_detail),
+    path('user/<int:user_id>/', views.user_detail),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,68 +1,53 @@
-from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse, HttpResponse
-
-from .serializers import TodoSerializer, UserSerializer
-from .models import Todo, User
-
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .models import Todo, User
+from .serializers import TodoSerializer, UserSerializer
 
 
 @api_view(['POST'])
 @permission_classes((IsAuthenticated, ))
-@authentication_classes((JSONWebTokenAuthentication, ))
+@authentication_classes((JWTAuthentication, ))
 def todo_create(request):
-
     serializer = TodoSerializer(data=request.POST)
-
     if serializer.is_valid():
         serializer.save()
-
         return JsonResponse(serializer.data)
-
     return HttpResponse(status=400)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes((IsAuthenticated, ))
-@authentication_classes((JSONWebTokenAuthentication, ))
+@authentication_classes((JWTAuthentication, ))
 def todo_detail(request, todo_id):
-
     todo = get_object_or_404(Todo, id=todo_id)
-
     if request.method == 'GET':
         serializer = TodoSerializer(todo)
-
         return JsonResponse(serializer.data)
     elif request.method == 'PUT':
         serializer = TodoSerializer(todo, request.data)
-
         if serializer.is_valid():
             serializer.save()
-
             return JsonResponse(serializer.data)
-
         return HttpResponse(status=400)
     elif request.method == 'DELETE':
         todo.delete()
-        
         return HttpResponse(status=204)
 
 
 @api_view(['GET'])
 @permission_classes((IsAuthenticated, ))
-@authentication_classes((JSONWebTokenAuthentication, ))
+@authentication_classes((JWTAuthentication, ))
 def user_detail(request, user_id):
-
     user = get_object_or_404(User, id=user_id)
-
     if request.user != user:
         return HttpResponse(status=403)
 
     if request.method == 'GET':
         serializer = UserSerializer(user)
-
         return JsonResponse(serializer.data)
+
+    return HttpResponse(status=400)
