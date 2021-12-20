23e73912d111f0a5fddd0443d31fa9d1d201f521
myeonghan-nim <ataraxia9408@gmail.com>
2021-12-20 14:57:09 +0900

    Fix: JWT library

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', obtain_jwt_token),
+    path('api-token-auth/', TokenObtainPairView.as_view()),
     path('api/v1/', include('todo.urls')),
 ]
